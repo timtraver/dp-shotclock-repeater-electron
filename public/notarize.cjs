e4fc6571836a654d3497b76d3ba2e6e35fc16620
Tim Traver <timtraver@gmail.com>
2024-04-01 19:09:17 -0700

    Updated notarize script to have tool specified by apple

--- public/notarize.cjs
+++ public/notarize.cjs
@@ -9,10 +9,12 @@ exports.default = async function notarizing(context) {
 
     const appName = context.packager.appInfo.productFilename;
     const data = {
+        tool: 'notarytool',
         appBundleId: 'com.digitalpool.shot.clock.repeater',
         appPath: `${appOutDir}/${appName}.app`,
         appleId: process.env.APPLE_ID,
-        appleIdPassword: process.env.APPLE_ID_PASSWORD
+        appleIdPassword: process.env.APPLE_ID_PASSWORD,
+        teamId: process.env.TEAM_ID
     }
     console.log("data : ", data);
     return await notarize(data);
